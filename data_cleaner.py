df3aac37a017bed49cad30bad56c4ae4bf77f6f5
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2021-06-17 11:17:34 +0100

    And more

--- data_cleaner.py
+++ data_cleaner.py
@@ -7,9 +7,10 @@ class DataCleaner:
 
     def remove_noise(text):
         #print(f"Before clean: {text}")
-        text = re.sub('\[.*?\]<.*?>+', '', text)
+        #text = re.sub('@[\w]+','',text)     #remove usernames - not sure if needed
+        text = re.sub('\[.*?\]', '', text)
         text = re.sub('https?://\S+|www\.\S+', '', text)
-        #text = re.sub('<.*?>+', '', text)
+        text = re.sub('<.*?>+', '', text)
         text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
         text = re.sub('\n', ' ', text)
         text = re.sub('\w*\d\w*', '', text)
---
+++ test3.py
@@ -0,0 +1,9 @@
+from country import Country
+
+def main():
+    c = Country()
+    c.save("United States")
+
+
+if __name__ == "__main__":
+    main()
